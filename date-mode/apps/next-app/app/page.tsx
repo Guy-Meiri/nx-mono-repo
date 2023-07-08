413e41141ab6250f4d2bed8f700aa87c9c02e2e4
guymeiri <guymeiri@microsoft.com>
2023-07-08 13:01:27 +0300

    aa

--- date-mode/apps/next-app/app/page.tsx
+++ date-mode/apps/next-app/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Index() {
         <div className="container">
           <div id="welcome">
             <h1>
-              <span> Hello there 5 </span>
+              <span> Hello there 6 </span>
               Welcome next-app 👋
             </h1>
           </div>
