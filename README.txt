e15a7e19d5d3209d97436fad90dfc2a43d78ae7a
guymeiri <guymeiri@microsoft.com>
2023-07-08 12:41:19 +0300

    add next-js-app

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
-Steps:
+Creation steps:
 
-1.npx create-nx-workspace
+1.npx create-nx-workspace with express server
 
 √ Where would you like to create your workspace? · date-mode
 √ Which stack do you want to use? · node
@@ -9,3 +9,7 @@ Steps:
 √ Application name · express-server
 √ Would you like to generate a Dockerfile? [https://docs.docker.com/] · No
 √ Enable distributed caching to make your CI faster · No
+
+- run express server:
+nx run express-server:serve
+
--- date-mode/apps/express-server/src/main.ts
+++ date-mode/apps/express-server/src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { pokemon } from './pokemon';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -6,7 +7,18 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send(pokemon);
+});
+
+app.get('/search', (req, res) => {
+  const q = req.query.q;
+  res.send(
+    pokemon.filter((p) =>
+      p.name.english
+        .toLocaleLowerCase()
+        .includes(((q as string) ?? '').toLowerCase())
+    )
+  );
 });
 
 app.listen(port, host, () => {
